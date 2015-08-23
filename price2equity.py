72b2bcd63aa89611de307d595252b4becca25da4
nokko <nsinnhuber@gmail.com>
2015-08-23 00:52:44 -0400

    abs path testing

--- price2equity.py
+++ price2equity.py
@@ -4,11 +4,11 @@ import time
 from datetime import datetime 
 
 
-path = os.path.join(os.getcwd(), "/Data/intraQuarter/")
+path = "/Users/Nikolas/Documents/Programming/Projects/2 Much learn/ana/Data/intraQuarter/_KeyStats"
 
 def Key_Stats(gather = "Total Debt/Equity (mrq)"):
-	statspath = os.path.join('_KeyStats')
-	stock_list = [x[0] for x in os.walk(statspath)]
+	statspath = path
+	stock_list = [x[0] for x in os.walk(path)]
 	print(stock_list)
 
 Key_Stats()
